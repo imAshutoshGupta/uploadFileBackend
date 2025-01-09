5c5544930d42583c53aadda7d7f0b14af6cff074
imAshutoshGupta <aguptaworkspace@gmail.com>
2025-01-09 17:58:32 +0530

    prod ready

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,14 +1,14 @@
-const Document = require('../models/documentModel')
+const Document = require('../models/documentModel');
+
 exports.getDashboard = async (req, res) => {
     try {
-        const files = await Document.find().populate('user_id', 'email username').sort({ createdAt: -1 })
-        if (!files || files.length === 0) {
-            return res.status(404).json({ message: 'No files found' })
-        }
+        const files = await Document.find({ user_id: req.user.userId })
+            .populate('user_id', 'email username')
+            .sort({ createdAt: -1 });
 
-        res.status(200).json(files)
+        res.status(200).json(files || []);
     } catch (error) {
-        console.error('Error fetching files:', error)
-        res.status(500).json({ message: 'Error fetching files' })
+        console.error('Error fetching files:', error);
+        res.status(500).json({ message: 'Error fetching files' });
     }
-}
+};
