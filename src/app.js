036dd8711524d004b947cdfa56433738835f567b
imAshutoshGupta <aguptaworkspace@gmail.com>
2025-01-09 14:19:00 +0530

    bug fix cors

--- src/app.js
+++ src/app.js
@@ -7,18 +7,19 @@ const adminRoute = require('./routes/admin.js')
 
 const app = express()
 
-app.use(express.json())
-app.use(cookieParser())
-
 const corsOptions = {
     origin: ['http://localhost:5173', 'https://upload-file-frontend-rust.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     credentials: true, 
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization'], 
 }
 
 app.use(cors(corsOptions))
 app.options('*', cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser()) 
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
