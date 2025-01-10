aebe91faabec8fd795c89404ad58fbed905f13b3
imAshutoshGupta <aguptaworkspace@gmail.com>
2025-01-10 14:52:27 +0530

    cors debugging

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(cors({
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://cloud-vault-sigma.vercel.app')
   res.header('Access-Control-Allow-Credentials', 'true')
+  console.log('Headers set for CORS debugging.')
   next()
 })
 
