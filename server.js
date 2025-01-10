e2611933263ddf44d8e963eae4db381a27bc0908
imAshutoshGupta <aguptaworkspace@gmail.com>
2025-01-10 14:29:09 +0530

    trying to fix cors

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const app = require('./src/app')
+const express = require('express')
+const cors = require("cors")
 const mongoose = require('mongoose')
 require('dotenv').config()
+
+const app = express()
+
+app.use(cors({
+    origin: 'https://cloud-vault-sigma.vercel.app', 
+    credentials: true, 
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], 
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+}))
 
 const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URI)
--- src/app.js
+++ src/app.js
@@ -17,12 +17,6 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
 app.use('/auth', authRoutes)
 app.use('/dashboard', dashboardRoute)
 app.use('/admin', adminRoute)
