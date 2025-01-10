d8d57678a369dbb28735766256f1073b3b3eeebd
imAshutoshGupta <aguptaworkspace@gmail.com>
2025-01-10 21:29:50 +0530

    origin bugs

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.send();
+  res.sendStatus(200)
 });
 
 app.use((req, res, next) => {
